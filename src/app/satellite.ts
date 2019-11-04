112a4a4587211c1f0292f4989c579951bf66a7ba
Ashley Downs <ashleydowns@Ashleys-MacBook-Air.local>
2019-11-03 21:04:32 -0600

    finished

---
+++ src/app/satellite.ts
@@ -0,0 +1,27 @@
+export class Satellite {
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    this.name = name;
+    this.orbitType = orbitType;
+    this.type = type;
+    this.operational = operational;
+    this.launchDate = launchDate
+
+    
+}
+shouldShowWarning(){
+    let checkDebris = this.type.toLowerCase()
+    if (checkDebris === 'space debris'){
+        return true;
+    } else {
+        return false
+    }
+    
+    
+}
+}
